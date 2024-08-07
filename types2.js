627e504225ce13cb689d2e1f962d002ac3de85e2
Andy Lu <andu.l@dottomesh.com>
2024-08-07 17:04:54 +0800

    feat: types2 補充註解, 在出一道涵數題目.

--- types2.js
+++ types2.js
@@ -10,6 +10,9 @@ let Null = null;        // 宣告並賦予空值 (推薦)
 console.log(`空值: ${Null}`);
 
 
+console.log();
+
+
 // 2. Array 陣列
 // 用途: 在資料數目是多個(1或0或n)的情況下, 如果一個一個宣告會不好閱讀與管理
 // 在這組資料的用途目的是一致的狀況下, 我們會用 陣列 來宣告他
@@ -24,31 +27,48 @@ console.log(`秀出分數陣列的長度 => ${gradeList.length}`);  //後面加 
 
 console.log(`把第一位同學的分數換成 100 分 => ${gradeList[0] = 100}`);
 console.log(`增加一位考90分的同學到陣列裡 ${gradeList.push(90)} => ${gradeList[5]}`);   
-// 詳細陣列的操作方法可以參考 https://developer.mozilla.org/zh-TW/docs/Web/JavaScript/Reference/Global_Objects/Array
+// 詳細陣列物件的操作方法可以參考 https://developer.mozilla.org/zh-TW/docs/Web/JavaScript/Reference/Global_Objects/Array
 
 console.log();
 
 // 3. Function 函數 (處理邏輯的資料型態)
 // 用途: 用來封裝邏輯運算, 藉由規範好輸入與輸出, 把邏輯抽象化.
 // Ex: 今天有一個函數 他可以把兩個字串的長度相加起來並返回出來.
+// 第一種宣告方式
 function subTwoStringLength(str1, str2) {
     return str1.length - str2.length;
 };
 console.log(`計算 'SKT' 與 'T1' 的長度差: ${subTwoStringLength('SKT', 'T1')}`);
 
+// 第二種宣告方式
 let sumTwoStringLength = function(str1, str2) {
     return str1.length + str2.length;
 };
 console.log(`計算 'SKT' 與 'T1' 的長度和: ${sumTwoStringLength('SKT', 'T1')}`);
 
+// 使用箭頭函數的宣告方式
+let multiTwoStringLength2 = (str1, str2) => {
+    return str1.length * str2.length;
+};
+console.log(`計算 'SKT' 與 'T1' 的長度積: ${multiTwoStringLength2('SKT', 'T1')}`);
+
 
 console.log();
 
-// 作業: 輸入兩個數字x, y, 回傳 x 乘上 y 再加上 x 除 y 的餘數.
+
+// !!!!!!!!!! 作業 !!!!!!!!!!
+// 輸入兩個數字x, y, 回傳 x 乘上 y 再加上 x 除 y 的餘數.
 function homework(x, y) {
     return x * y + x % y;
 }
 console.log(`計算3乘上2再加上3除2的餘數: ${homework(3, 2)}`);
+
+// 輸入一個字串, 長度為奇數則回傳長度的平方, 長度為偶數則回傳長度的倒數, 0 就回傳 0.
+
+
+
+console.log();
+
 
 // 4. 物件
 
